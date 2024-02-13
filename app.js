3ca9ef0b61673379465d9f0ec5d986123a30e3bc
wootaepark <pakdal99@gmail.com>
2024-02-13 14:49:33 +0900

    first_commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// 내가 구상하고 있는 어플리케이션 : 유튜브 (링크)혹은 자신이 mp3 파일을 올릴 수도 있다.
+
+// 여러 사람들이 음악을 공유하고 별점 혹은 댓글을 달 수 있다.
+
+// 좋아요 혹은 별점에 따른 순위를 볼 수 있도록 하는 것이 이 프로젝트의 목표
