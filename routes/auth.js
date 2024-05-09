1ac6e6553690c6833960537dbf724006f43df2dc
wootaepark <pakdal99@gmail.com>
2024-05-09 20:19:52 +0900

    add-comments-7

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const {join,login,logout} = require('../controllers/auth');
 
 const router = express.Router();
 
+// isNotLoggedIn 미들웨어를 이용하여 로그인 경로 구현
+
 router.post('/join', isNotLoggedIn, join);
 router.post('/login', isNotLoggedIn, login);
 router.get('/logout', isLoggedIn, logout);
